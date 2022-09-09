46d72f2db05f5ccb2680bbec3943da953f7f04f9
Juan Esteban Yepes <juan.yepesq@comunidad.iush.edu.co>
2022-09-09 15:03:53 -0500

    se implimento un documento basico de html y conceptos basicos de javascript

---
+++ trabajos/codigo.js
@@ -0,0 +1,107 @@
+// tipo de datos
+/*string = "cadena de texto"
+number = 19
+booleano = false*/
+
+
+//------------tipo de variables------------
+//funcion de las variable var, let, const 
+// trabajan con bloques
+/*var = "para inicializar una variable"
+let = "nos limita el alcance de la variable al bloque que lo utilizamos"
+const = "es una variable que no puede cambiar su valor"*/
+
+//ejemplo
+/*let numero = 29;
+numero = 23
+alert(numero)*/
+
+//-------------multiples variables------
+//ejemplo1
+/*let numero, numero2, numero3;
+
+numero = 2;
+numero2 = 9;
+numero3 = 13;
+
+alert(numero);
+alert(numero2);
+alert(numero3);*/
+
+//ejemplo 2
+/*let numero1 = 23;
+let numero2 = 39;*/
+
+
+//-----------casos especiales de datos: undefined, null, Nan;---------------------
+
+/*let numero = null // nos dice que variables esta vacia
+
+let numero1 = 5;
+let numero2 = "pedro";
+
+alert(numero1 * numero2); //resultado NaN (cuando se intenta hacer algo que no es un numero)*/
+
+
+//------------pruebas con prompt------------------
+
+//prompt("decime tu nombre");
+//ejemplo1
+
+/*let nombre = prompt("decime tu nombre");
+
+alert(nombre);
+
+//ejemplo 2
+let nombre = prompt("dime tu nombre");
+
+alert("hola " + nombre);*/
+
+
+
+//----------operadores aritmeticos------------
+//ejemplo1
+/*numero1 = 10;
+numero2 = 5;
+
+resultado = numero1 + numero2;
+
+alert(resultado);*/
+
+
+//ejemplo 2
+/*numero1 = 10;
+numero2 = 5;
+
+//numero1--; // decremento
+//resultado = numero1/ numero2;
+//resultado = numero1**2; // se hace multiplica al cuadrado
+//resultado = numero1**3; // se multiplica al cubo
+//resultado = numero1**numero2; // se eleva 5 veces
+//numero1++;
+//resultado = numero1; // incremento
+//resultado = numero1&numero2; // el resto de 10 / 5 =0
+
+
+alert(resultado);*/
+
+//----------operadores de asignacion---------
+
+/*let numero = 10;
+//numero += 5;
+//numero -= 5;
+//numero *= 5;
+//numero /= 5;
+//numero %= 6;
+//numero **=2;// exponente
+
+
+document.write(numero); */
+
+
+//---------------concatencion-------------
+//minuto del video 1:03:18
+
+
+
+
